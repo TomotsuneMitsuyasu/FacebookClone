b53a207c596503b1b3bb8713b5588c52c3b09a52
tomotsunemitsuyasu <beckn2149@gmail.com>
2019-08-11 17:17:27 +0900

    pictures routing修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'pictures/index'
   # get 'sessions/new'
   resources :users
-  resources :pictures
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
   resources :sessions,only:[:new,:create,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
